dc3ee8a1026dffa142ee991f2af03035a2b68a75
unknown <ratiporn.pr@gmail.com>
2023-10-26 00:37:18 +0700

    feat: form validate

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import Warning from "./warning";
 
 function Form({ onAddProduct }) {
   const [name, setName] = useState("");
@@ -7,6 +8,13 @@ function Form({ onAddProduct }) {
   const [price, setPrice] = useState("");
   const [quantity, setQuantity] = useState(1);
   const [unit, setUnit] = useState("ชิ้น");
+  const [isNameError, setIsNameError] = useState(false);
+  const [isPriceError, setIsPriceError] = useState(false);
+  const [isQuantityError, setIsQuantityError] = useState(false);
+  const [isNameExist, setIsNameExist] = useState(false);
+  const [isPriceExist, setIsPriceExist] = useState(false);
+  const [isQuantityExist, setIsQuantityExist] = useState(false);
+  const [isUnitExist, setIsUnitExist] = useState(false);
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -28,9 +36,44 @@ function Form({ onAddProduct }) {
       console.log(error);
     }
 
-    //input validation
-    if (!name || !price || !quantity || !unit) return;
-    if (name.length > 20) return;
+    //name character must not exceed 20
+    setIsNameError(name.length > 20 ? true : false);
+
+    //price and quantity have to be number
+    function isNumber(s) {
+      const pattern = /^\d+$/;
+      return pattern.test(s);
+    }
+
+    if (!isNumber(price) && price.length > 0) {
+      setIsPriceError(true);
+    } else {
+      setIsPriceError(false);
+    }
+
+    if (!isNumber(quantity) && quantity.length > 0) {
+      setIsQuantityError(true);
+    } else {
+      setIsQuantityError(false);
+    }
+
+    //all inputs must exist
+    setIsNameExist(!name ? true : false);
+    setIsPriceExist(!price ? true : false);
+    setIsQuantityExist(!quantity || quantity === 0 ? true : false);
+    setIsUnitExist(!unit ? true : false);
+
+    if (
+      !name ||
+      !price ||
+      !quantity ||
+      quantity === 0 ||
+      !unit ||
+      name.length > 20 ||
+      !isNumber(quantity) ||
+      !isNumber(price)
+    )
+      return;
 
     const newProduct = {
       id: Date.now(),
@@ -61,13 +104,57 @@ function Form({ onAddProduct }) {
   return (
     <div className="">
       <form className="form-wrapper" onSubmit={handleSubmit}>
-        <label htmlFor="name">ชื่อ</label>
-        <input
-          id="name"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        ></input>
+        {/* validation message */}
+
+        {isNameError && (
+          <div class="container" id="name-error">
+            <div class="arrow">
+              <div class="outer"></div>
+              <div class="inner"></div>
+            </div>
+            <div className="message-body">
+              <p>โปรดใส่ชื่อไม่เกิน 20 ตัวอักษร</p>
+            </div>
+          </div>
+        )}
+
+        {isPriceError && (
+          <div class="container" id="price-error">
+            <div class="arrow">
+              <div class="outer"></div>
+              <div class="inner"></div>
+            </div>
+            <div className="message-body">
+              <p>โปรดใส่ตัวเลข</p>
+            </div>
+          </div>
+        )}
+
+        {isQuantityError && (
+          <div class="container" id="quantity-error">
+            <div class="arrow">
+              <div class="outer"></div>
+              <div class="inner"></div>
+            </div>
+            <div className="message-body">
+              <p>โปรดใส่ตัวเลข</p>
+            </div>
+          </div>
+        )}
+
+        <div style={{ position: "relative" }}>
+          <label htmlFor="name">ชื่อ</label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+              setIsNameExist(false);
+            }}
+          ></input>
+          {isNameExist && <Warning />}
+        </div>
 
         <label htmlFor="img">รูป</label>
         <div className="file-input-container">
@@ -83,29 +170,49 @@ function Form({ onAddProduct }) {
           ></input>
         </div>
 
-        <label htmlFor="price">ราคา (ชิ้น)</label>
-        <input
-          id="price"
-          type="text"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-        ></input>
+        <div style={{ position: "relative" }}>
+          <label htmlFor="price">ราคา (ชิ้น)</label>
+          <input
+            id="price"
+            type="text"
+            value={price}
+            onChange={(e) => {
+              setPrice(e.target.value);
+              setIsPriceExist(false);
+            }}
+          ></input>
+          {isPriceExist && <Warning />}
+        </div>
 
-        <label htmlFor="quantity">จำนวน</label>
-        <input
-          id="quantity"
-          type="number"
-          value={quantity}
-          onChange={(e) => setQuantity(e.target.value)}
-        ></input>
+        <div style={{ position: "relative" }}>
+          <label htmlFor="quantity">จำนวน</label>
+          <input
+            id="quantity"
+            type="number"
+            value={quantity}
+            onChange={(e) => {
+              setQuantity(e.target.value);
+              setIsQuantityExist(false);
+            }}
+            onMouseEnter={() => setIsQuantityExist(false)}
+            min="1"
+          ></input>
+          {isQuantityExist && <Warning />}
+        </div>
 
-        <label htmlFor="unit">หน่วย</label>
-        <input
-          id="unit"
-          type="text"
-          value={unit}
-          onChange={(e) => setUnit(e.target.value)}
-        ></input>
+        <div style={{ position: "relative" }}>
+          <label htmlFor="unit">หน่วย</label>
+          <input
+            id="unit"
+            type="text"
+            value={unit}
+            onChange={(e) => {
+              setUnit(e.target.value);
+              setIsUnitExist(false);
+            }}
+          ></input>
+          {isUnitExist && <Warning />}
+        </div>
 
         <div>
           <button id="send-button">Send</button>

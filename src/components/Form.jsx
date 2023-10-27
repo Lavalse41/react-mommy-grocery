296ab37c7534f656c33ca88aca22ebca90ebf410
unknown <ratiporn.pr@gmail.com>
2023-10-27 13:41:36 +0700

    fix: warning component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import Warning from "./warning";
+import Warning from "./Warning";
 
 function Form({ onAddProduct }) {
   const [name, setName] = useState("");
